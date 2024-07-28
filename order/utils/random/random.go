bdd976d72078189724d5a377336afef1a951b334
Amir <assassinmoltafet@gmail.com>
2024-07-28 17:24:04 +0330

    include random package

---
+++ order/utils/random/random.go
@@ -0,0 +1,25 @@
+package random
+
+import (
+	"crypto/rand"
+	"math"
+	"math/big"
+)
+
+func generateRandomNumber(length int) int {
+	min := int64(math.Pow(10, float64(length)-1))
+	max := int64(math.Pow(10, float64(length))) - 1
+
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(max-min))
+	if err != nil {
+		panic(err)
+	}
+
+	number := int(randomNumber.Int64()) + int(min)
+
+	return number
+}
+
+func GenerateUserID() int {
+	return generateRandomNumber(8)
+}
