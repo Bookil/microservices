3d23e3055ca6b02d229a00f63226bcf3522e2128
Amir <assassinmoltafet@gmail.com>
2024-08-26 22:47:56 +0330

    debug api port

--- user/internal/ports/api.go
+++ user/internal/ports/api.go
@@ -2,8 +2,8 @@ package ports
 
 import "context"
 
-type API interface {
-	Register(ctx context.Context, firstName, lastName, email, password string) error
+type APIPort interface {
+	Register(ctx context.Context, firstName, lastName, email, password string) (string,error)
 	Login(ctx context.Context, email, password string) (string, string, error)
 	VerifyEmail(ctx context.Context,email string, code string) error
 	ChangePassword(ctx context.Context,oldPassword, newPassword string) error
