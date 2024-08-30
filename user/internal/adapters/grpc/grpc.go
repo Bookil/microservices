8b97a08d98c920d3b460bb4efbc32715347047de
Amir <assassinmoltafet@gmail.com>
2024-08-30 22:36:53 +0330

    implement grpc adapter

---
+++ user/internal/adapters/grpc/grpc.go
@@ -0,0 +1,72 @@
+package grpc
+
+import (
+	"context"
+
+	userv1 "github.com/Bookil/Bookil-Proto/gen/golang/user/v1"
+	"github.com/Bookil/microservices/user/internal/adapters/grpc/interceptor"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (a *Adapter) Register(ctx context.Context, request *userv1.RegisterRequest) (*userv1.RegisterResponse, error) {
+	userID, err := a.api.Register(ctx, request.FisrtName, request.LastName, request.Email, request.Password)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &userv1.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func (a *Adapter) Login(ctx context.Context, request *userv1.LoginRequest) (*userv1.LoginResponse, error) {
+	accessToken, refreshToken, err := a.api.Login(ctx, request.Email, request.Password)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &userv1.LoginResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
+
+func (a *Adapter) ChangePassword(ctx context.Context, request *userv1.ChangePasswordRequest) (*userv1.ChangePasswordResponse, error) {
+	userID, ok := ctx.Value(interceptor.UserID{}).(string)
+	if !ok {
+		return nil, status.Error(codes.Internal, "an error occurred")
+	}
+	err := a.api.ChangePassword(ctx, userID, request.NewPassword, request.OldPassword)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &userv1.ChangePasswordResponse{}, nil
+}
+
+func (a *Adapter) Update(ctx context.Context, request *userv1.UpdateRequest) (*userv1.UpdateResponse, error) {
+	userID, ok := ctx.Value(interceptor.UserID{}).(string)
+	if !ok {
+		return nil, status.Error(codes.Internal, "an error occurred")
+	}
+	err := a.api.Update(ctx, userID, request.NewFirstName, request.NewLastName)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &userv1.UpdateResponse{}, nil
+}
+
+func (a *Adapter) DeleteAccount(ctx context.Context, request *userv1.DeleteAccountRequest) (*userv1.DeleteAccountResponse, error) {
+	userID, ok := ctx.Value(interceptor.UserID{}).(string)
+	if !ok {
+		return nil, status.Error(codes.Internal, "an error occurred")
+	}
+	err := a.api.DeleteAccount(ctx, userID, request.Password)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &userv1.DeleteAccountResponse{}, nil
+}
---
+++ user/internal/adapters/grpc/interceptor/authentication.go
@@ -0,0 +1,56 @@
+package interceptor
+
+import (
+	"context"
+
+	"github.com/Bookil/microservices/user/internal/ports"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+const accessTokenHeader = "X-Access-Token"
+
+type UserID struct{}
+
+type AuthInterceptor struct {
+	auth ports.AuthPort
+}
+
+func NewAuthInterceptor(auth ports.AuthPort) *AuthInterceptor{
+	return &AuthInterceptor{
+		auth: auth,
+	}
+}
+
+func (a *AuthInterceptor) AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// List of methods that require authentication
+	authMethods := map[string]bool{
+		"/userv1.UserService/Update":         true,
+		"/userv1.UserService/ChangePassword": true,
+	}
+
+	if authMethods[info.FullMethod] {
+		// Perform authentication logic here
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, status.Errorf(codes.Unauthenticated, "missing metadata")
+		}
+
+		if token, ok := md[accessTokenHeader]; ok && len(token) > 0 {
+			userID, err := a.auth.Authenticate(ctx, token[0])
+			if err != nil {
+				return nil, status.Errorf(codes.Unauthenticated, err.Error())
+			}
+			if len(userID) > 0 {
+				ctx = context.WithValue(ctx, UserID{}, userID)
+				return handler(ctx, req)
+			}
+		}
+
+		return nil, status.Errorf(codes.Unauthenticated, "access dined")
+	}
+
+	return handler(ctx, req)
+}
