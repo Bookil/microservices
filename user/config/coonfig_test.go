bdc8b3ded6368f5abadc5f5063a36de146d4b2d1
Amir <assassinmoltafet@gmail.com>
2024-09-20 16:53:46 +0330

    refactor

--- user/config/coonfig_test.go
+++ user/config/coonfig_test.go
@@ -1,6 +1,8 @@
 package config_test
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"testing"
 
@@ -9,32 +11,45 @@ import (
 )
 
 func TestDevelopmentConfig(t *testing.T) {
-	os.Setenv("USER_ENV", "development")
+	err := checkAndSet("development")
+	if err != nil {
+		if isFileNotExists(err) {
+			return
+		}
+		require.Error(t, err)
+	}
 
 	configs := config.Read()
 	require.NotEmpty(t, configs)
 	require.Equal(t, config.CurrentEnv, config.Development)
 }
 
+func TestProductionConfig(t *testing.T) {
+	err := checkAndSet("production")
+	if err != nil {
+		if isFileNotExists(err) {
+			return
+		}
+		require.Error(t, err)
+	}
+
+	configs := config.Read()
+	require.NotEmpty(t, configs)
+	require.Equal(t, config.CurrentEnv, config.Production)
+}
+
 func TestTestConfig(t *testing.T) {
-	os.Setenv("USER_ENV", "test")
+	err := checkAndSet("test")
+	if err != nil {
+		if isFileNotExists(err) {
+			return
+		}
+		require.Error(t, err)
+	}
 
 	configs := config.Read()
 	require.NotEmpty(t, configs)
 	require.Equal(t, config.CurrentEnv, config.Test)
-}
-
-func TestProductionConfig(t *testing.T) {
-	os.Setenv("USER_ENV", "production")
-
-	defer func() {
-		r := recover()
-		if r == nil {
-			require.Fail(t, "should fatal")
-		}
-	}()
-
-	config.Read()
 }
 
 func TestInvalidEnv(t *testing.T) {
@@ -48,4 +63,21 @@ func TestInvalidEnv(t *testing.T) {
 	}()
 
 	config.Read()
+}
+
+func checkAndSet(env string) error {
+	_, err := os.Stat(fmt.Sprintf("config.%s.yml", env))
+	if err != nil {
+		return err
+	}
+
+	os.Setenv("USER_ENV", env)
+
+	return nil
+}
+
+func isFileNotExists(err error) bool {
+	isFileNotExists := errors.Unwrap(err).Error() == "no such file or directory"
+
+	return isFileNotExists
 }
