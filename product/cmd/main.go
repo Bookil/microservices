a37fae4aced2f0b8b16e4b7560609efdb780eb4a
Amir <assassinmoltafet@gmail.com>
2024-10-13 22:21:04 +0330

    include main.go

--- product/cmd/main.go
+++ product/cmd/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"product/config"
+	"product/internal/adapters/auth"
+	"product/internal/adapters/cart"
+	"product/internal/adapters/db"
+	"product/internal/adapters/db/mysql_adapter"
+	"product/internal/adapters/grpc"
+	"product/internal/application/core/api"
+)
+
+func main() {
+	configs := config.Read()
+
+	db,err := db.NewDB(&configs.Mysql)
+	checkError(err)
+
+	dbAdapter := mysql_adapter.NewAdapter(db)
+
+	cartAdapter,err := cart.NewAdapter(&configs.CartService)
+	checkError(err)
+
+	authAdapter,err := auth.NewAdapter(&configs.AuthService)
+	checkError(err)
+
+	application := api.NewApplication(cartAdapter,dbAdapter)
+
+	grpcAdapter := grpc.NewAdapter(application,authAdapter,configs.Server.Port)
+	
+	grpcAdapter.Run()
+}
+
+
+func checkError(err error){
+	if err != nil{
+		log.Fatal(err)
+	}
+}
